4f7f2947dfe85bd70eab5d2aa33bed93f41c8050
Francis Carr <francis.carr@guardian.co.uk>
2016-03-11 15:41:57 +0000

    Change LastSent to an ADT and use types for key events and last sent based on date

--- common/app/services/LastSentDatabase.scala
+++ common/app/services/LastSentDatabase.scala
@@ -15,8 +15,15 @@ import scala.collection.JavaConverters._
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
+sealed trait LastSent
 
-case class LastSent(topic: String, dateTime: DateTime)
+case class LastSentDateOnly(topic: String, dateTime: DateTime) extends LastSent
+
+object LastSentDateOnly {
+  def emptyForTopic(topic: String): LastSent = LastSentDateOnly(topic, DateTime.now())
+}
+
+case class LastSentKeyEvent(topic: String, dateTime: DateTime, keyEventId: Option[String]) extends LastSent
 
 object LastSent {
   implicit object LastSentDynamoFormat extends DynamoFormat[LastSent] {
@@ -25,21 +32,48 @@ object LastSent {
         Option(value.getM).map(_.asScala.toMap)
           .getOrElse(Map.empty)
 
-      for {
-        topic <- valueMap.get("topic").flatMap(av => Option(av.getS))
-        dateTime <- valueMap.get("dateTime")
-          .flatMap(av => Option(av.getN))
-          .map(_.toLong)
-          .map(l => new DateTime(l * 1000))
-      } yield LastSent(topic, dateTime)
+      valueMap.get("type").flatMap(av => Option(av.getS)) match {
+        case Some("lastSentDateOnly") =>
+          for {
+            topic <- valueMap.get("topic").flatMap(av => Option(av.getS))
+            dateTime <- valueMap.get("dateTime")
+              .flatMap(av => Option(av.getN))
+              .map(_.toLong)
+              .map(l => new DateTime(l * 1000))
+          } yield LastSentDateOnly(topic, dateTime)
+        case Some("lastSentKeyEvent") =>
+          for {
+            topic <- valueMap.get("topic").flatMap(av => Option(av.getS))
+            dateTime <- valueMap.get("dateTime")
+              .flatMap(av => Option(av.getN))
+              .map(_.toLong)
+              .map(l => new DateTime(l * 1000))
+            keyEvent = valueMap.get("keyEvent").flatMap(av => Option(av.getS))
+          } yield LastSentKeyEvent(topic, dateTime, keyEvent)
+        case _ => None
+      }
+
+
     }
 
     override def write(value: LastSent): AttributeValue = {
-      new AttributeValue().withM(
-        Map(
-          "topic" -> new AttributeValue().withS(value.topic),
-          "dateTime" -> new AttributeValue().withN((value.dateTime.getMillis / 1000).toString)
-        ).asJava)
+      value match {
+        case lastSentDateOnly: LastSentDateOnly =>
+          new AttributeValue().withM(
+            Map(
+              "type" -> new AttributeValue().withS("lastSentDateOnly"),
+              "topic" -> new AttributeValue().withS(lastSentDateOnly.topic),
+              "dateTime" -> new AttributeValue().withN((lastSentDateOnly.dateTime.getMillis / 1000).toString)
+            ).asJava)
+        case lastSentKeyEvent: LastSentKeyEvent =>
+          val attributeMap = Map(
+            "type" -> new AttributeValue().withS("lastSentKeyEvent"),
+            "topic" -> new AttributeValue().withS(lastSentKeyEvent.topic),
+            "dateTime" -> new AttributeValue().withN((lastSentKeyEvent.dateTime.getMillis / 1000).toString))
+
+          new AttributeValue().withM(
+            lastSentKeyEvent.keyEventId.fold(attributeMap)(s => attributeMap + ("keyEvent" -> new AttributeValue().withS(s))).asJava)
+      }
     }
   }
 }
