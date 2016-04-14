8fe6db3909fd6a77223788861e587748fcabc84a
Francis Carr <francis.carr@guardian.co.uk>
2016-04-14 17:19:46 +0100

    Update capi-event-worker Healthcheck to use new metrics

--- capieventworker/app/controllers/Healthcheck.scala
+++ capieventworker/app/controllers/Healthcheck.scala
@@ -2,12 +2,23 @@ package controllers
 
 import javax.inject.Singleton
 
+import org.joda.time.DateTime
 import play.api.mvc.{Controller, Action}
+import services.ServerStatistics
 
 @Singleton
 class Healthcheck extends Controller {
 
+  val ConsecutiveErrorThreshold: Int = 10
+
   def healthcheck() = Action {
-    Ok("Ok")
+
+    if (ServerStatistics.lastCapiEventReceived.get.exists(lastReceived => DateTime.now.minusHours(1).isBefore(lastReceived))) {
+      InternalServerError(s"Have not successfully received a CAPI event since ${ServerStatistics.lastCapiEventReceived}")
+    } else if (ServerStatistics.thriftDeserialisationFailures.get >= ConsecutiveErrorThreshold) {
+      InternalServerError("Too many consecutive thrift parsing errors")
+    } else {
+      Ok("Everything is fine")
+    }
   }
 }
